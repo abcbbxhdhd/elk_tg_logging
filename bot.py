5a31b0cdc9e2280dd7e1a5b68e046d8cb2471a14
abcbbxhdhd <ajax76@protonmail.com>
2023-05-28 21:49:40 +0300

    Updated bot to push logs into CloudWatch

--- bot.py
+++ bot.py
@@ -2,16 +2,26 @@ import telebot
 from dotenv import dotenv_values
 import json
 import logging
-from logstash_async.handler import AsynchronousLogstashHandler
+from cloudwatch import cloudwatch
 
 config = dotenv_values(".env")
-host = config.get("LOGSTASH_HOST")
-port = int(config.get("LOGSTASH_PORT"))
+region = config.get("REGION")
 bot_key = config.get("BOT_KEY")
+aws_access_key_id = config.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = config.get("AWS_SECRET_ACCESS_KEY")
+log_group = config.get("LOG_GROUP")
+log_stream = config.get("LOG_STREAM")
 
-logger = logging.getLogger('bot-logging')
+handler = cloudwatch.CloudwatchHandler(
+    log_group=log_group,
+    access_id=aws_access_key_id,
+    access_key=aws_secret_access_key,
+    region=region,
+    log_stream=log_stream
+)
+logger = logging.getLogger('bot_logger')
 logger.setLevel(logging.INFO)
-async_handler = AsynchronousLogstashHandler(host, port, database_path=None)
+logger.addHandler(handler)
 
 bot = telebot.TeleBot(bot_key, parse_mode=None)
 
@@ -23,7 +33,7 @@ def echo_all(message):
         "fromUsername": message.from_user.username,
         "message": message.text
     }
-    print(json.dumps(messageInfo))
+    logger.info(json.dumps(messageInfo, ensure_ascii=False))
     bot.reply_to(message, "Your message has been received!")
 
 
