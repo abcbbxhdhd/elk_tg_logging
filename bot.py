8eac962eb9255a5ad0931d17de8c8e74c5a54d02
abcbbxhdhd <ajax76@protonmail.com>
2023-05-29 00:40:40 +0300

    Implemented /start message monitoring with Prometheus

--- bot.py
+++ bot.py
@@ -1,40 +1,23 @@
 import telebot
 from dotenv import dotenv_values
-import json
-import logging
-from cloudwatch import cloudwatch
+from prometheus_client import start_http_server, Counter
 
 config = dotenv_values(".env")
-region = config.get("REGION")
 bot_key = config.get("BOT_KEY")
-aws_access_key_id = config.get("AWS_ACCESS_KEY_ID")
-aws_secret_access_key = config.get("AWS_SECRET_ACCESS_KEY")
-log_group = config.get("LOG_GROUP")
-log_stream = config.get("LOG_STREAM")
 
-handler = cloudwatch.CloudwatchHandler(
-    log_group=log_group,
-    access_id=aws_access_key_id,
-    access_key=aws_secret_access_key,
-    region=region,
-    log_stream=log_stream
-)
-logger = logging.getLogger('bot_logger')
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
+start_counter = Counter('total_starts', "Total number of /start command")
 
 bot = telebot.TeleBot(bot_key, parse_mode=None)
 
 
 @bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    messageInfo = {
-        "chatId": message.chat.id,
-        "fromUsername": message.from_user.username,
-        "message": message.text
-    }
-    logger.info(json.dumps(messageInfo, ensure_ascii=False))
+def echo_all_and_monitor(message):
+    if message.text == "/start":
+        start_counter.inc()
+        bot.reply_to(message, "Hello. I'm here!")
+
     bot.reply_to(message, "Your message has been received!")
 
 
+start_http_server(9091)
 bot.infinity_polling()
